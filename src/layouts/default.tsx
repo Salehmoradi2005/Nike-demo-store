2eccbbe866e38d7351d77f7d9fbf61f183c9af43
Saleh Moradi <moradisaleh2@gmail.com>
2024-08-26 12:14:16 +0330

    update 8

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,6 +1,7 @@
 import IMG from "../../public/nike.ico";
-
+import { IoIosGlobe } from "react-icons/io";
 import { Navbar } from "@/components/navbar";
+import {FaInstagram , FaTelegram , FaTwitter , FaLinkedin} from 'react-icons/fa'
 
 export default function DefaultLayout({ children }: {children: React.ReactNode}) {
   return (
@@ -9,7 +10,7 @@ export default function DefaultLayout({ children }: {children: React.ReactNode})
       <main className={`container mx-auto max-w-7xl px-6 flex-grow pt-16 `}>
         {children}
       </main>
-      <footer className="w-full flex flex-col  justify-left px-16 py-3 S-footer  sm:px-40 sm:justify-center S-shadow">
+      <footer className=" flex flex-col  justify-left px-16 py-3 S-footer  sm:px-40 sm:justify-center S-shadow">
         <img src={IMG} className="sm:hidden" width={60} />
         <div className="w-100 flex flex-col justify-center py-3 S-footer justify-left gap-10 sm:flex-row sm:gap-40 ">
         <div className="S-footer-container">
@@ -39,6 +40,28 @@ export default function DefaultLayout({ children }: {children: React.ReactNode})
           <p className="font-semibold ">Yoga</p>
           <p className="font-semibold ">NikeLab</p>
         </div>
+        <div className="S-footer-container hidden md:hidden lg:block">
+          <h1 className="text-3xl font-bold mb-5">Clothing</h1>
+          <p className="font-semibold ">All Clothing</p>
+          <p className="font-semibold ">Tops & T-Shirts</p>
+          <p className="font-semibold ">Shorts</p>
+          <p className="font-semibold ">Hoodies</p>
+          <p className="font-semibold ">Socks</p>
+          <p className="font-semibold ">Yoga</p>
+          <p className="font-semibold ">NikeLab</p>
+        </div>
+        </div>
+        <div className="flex gap-5 my-10 text-3xl text-gray-500 md:mx-64 ">
+          <FaInstagram className="S-icon-hover transition-all"/>
+          <FaLinkedin className="S-icon-hover transition-all"/>
+          <FaTwitter className="S-icon-hover transition-all"/>
+          <FaTelegram className="S-icon-hover transition-all"/>
+        </div>
+        <h1 className="md:mx-64 md:text-xl flex items-center gap-2 text-gray-400 font-medium "><IoIosGlobe size={30}/> United State of America</h1>
+        <div className="mt-5">
+          <h1 className="md:mx-64 md:text-xl flex items-center gap-2 text-gray-400 font-medium "> About us</h1>
+          <h1 className="md:mx-64 md:text-xl flex items-center gap-2 text-gray-400 font-medium "> Contact Us</h1>
+          <h1 className="md:mx-64 md:text-xl flex items-center gap-2 text-gray-400 font-medium "> Copyright© 2024</h1>
         </div>
       </footer>
     </div>
