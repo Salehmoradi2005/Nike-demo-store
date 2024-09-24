0f162395decf80ca21634ccf01eee19b9e159796
Saleh Moradi <moradisaleh2@gmail.com>
2024-09-24 13:54:25 +0330

    M1

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -7,7 +7,7 @@ export default function Product({img , name , price , action , rating}) {
             <div className='relative'>
                 <h2 className='pl-2  text-md font-bold text-gray-500 w-max overflow-hidden sm:text-xl'>{name}</h2>
                 <h2 className='pl-2 text-lg mt-2  font-bold text-gray-400'>{price}$</h2>
-                <h2 className="flex items-center gap-1 pl-2 text-md  font-bold text-gray-400"><BiSolidStar  color="#f5a524" />{rating}</h2>
+                <h2 className="flex items-center gap-1 pl-2 text-sm  font-bold text-gray-400"><BiSolidStar  color="#f5a524" />{rating}</h2>
             </div>
             <div className='S-button'>
                     <p className='font-semibold text-gray-400'> View </p>
